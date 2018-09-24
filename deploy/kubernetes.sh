cd406f9c12dd8b61aa9336e860676870b348bc6d
jpiecuch <jakub.piecuch@jakubpiecuch.pl>
2018-09-24 09:00:49 +0200

    Splitting repositories

--- deploy/kubernetes.sh
+++ deploy/kubernetes.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl apply -f accounts/templates
+kubectl apply -f accounts/templates-kubernetes
--- deploy/openshift.sh
+++ deploy/openshift.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 oc apply -f accounts/templates -n gym-home
+oc apply -f accounts/templates-openshift -n gym-home
